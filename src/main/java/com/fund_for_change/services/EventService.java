c75a0944a0bfb2869cef14ee26302b74f9e41467
emon253 <emonchowdhury253@gmail.com>
2022-02-10 19:18:20 +0600

    first commit

---
+++ src/main/java/com/fund_for_change/services/EventService.java
@@ -0,0 +1,22 @@
+package com.fund_for_change.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.fund_for_change.domain.Event;
+import com.fund_for_change.dto.EventDTO;
+
+@Service
+public interface EventService {
+	public Event saveEvent(EventDTO eventDto);
+
+	public List<Event> getEvents();
+	
+	public Event getEventById(String id);
+
+	public Event updateEvent(Event event);
+
+	public void deleteEvent(Event event);
+
+}
